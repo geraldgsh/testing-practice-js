5ec8f4090d3b90a71d7839a02668c54bf875c618
geraldgsh <geraldgsh@hotmail.com>
2020-03-03 04:49:02 +0800

    more lint correction

--- capitalize/capitalize.js
+++ capitalize/capitalize.js
@@ -1,4 +1,5 @@
 /* eslint-disable linebreak-style */
+/*eslint arrow-body-style: ["error", "always"]*/
 
 const capitalize = (string) => {
   return string.charAt(0).toUpperCase() + string.substring(1);
--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,4 +1,5 @@
 /* eslint-disable linebreak-style */
+/*eslint arrow-body-style: ["error", "always"]*/
 
 const reverseString = (string) => {
   return string.split('').reverse().join('');
